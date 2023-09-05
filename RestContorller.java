a98234d29f2a337a2f8ec7c574b4de859411356c
BHUVANI-123 <97793556+BHUVANI-123@users.noreply.github.com>
2023-09-05 17:04:47 +0530

    Add files via upload

---
+++ RestContorller.java
@@ -0,0 +1,4 @@
+package com.example.springassesment;
+
+public @interface RestContorller {
+}
---
+++ UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.springassesment;
+
+
+import org.springframework.data.repository.CrudRepository;
+public interface UserRepository extends CrudRepository<User,Integer>{
+
+}
